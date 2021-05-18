f5d0de15c0a603b1df68087497e7c72532a7cdd6
«Aliz-TED» <>
2021-05-18 18:22:47 +0200

    push before reinstall dual boot

--- front/src/services/associations.js
+++ front/src/services/associations.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = "http://localhost:8000/api/";
+const url = toString(process.env.REACT_APP_API_ENDPOINT);
 
 const headersAuth = {
     'Access-Control-Allow-Origin': '*',
--- front/src/services/events.js
+++ front/src/services/events.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = "http://localhost:8000/api/";
+const url = process.env.REACT_APP_API_ENDPOINT;
 
 const headersAuth = {
     'Access-Control-Allow-Origin': '*',
